e4675719bef9dce42a708fd4a62e889cb53b2e52
NauvalHamzah <nauvalhamzah33@gmail.com>
2024-09-30 22:50:59 +0700

    Add Code

---
+++ FittingModel.py
@@ -0,0 +1,50 @@
+import numpy as np
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima.model import ARIMA
+
+
+source = pd.read_csv("Req_JB_BP.csv")
+source['Date'] = pd.to_datetime(source['Date'], format='%d/%m/%Y')
+
+Vehicle_Type = source.columns[1:]
+best_parameters = np.zeros((len(Vehicle_Type),7))
+
+counter=0
+for item in Vehicle_Type:
+    print(item)
+    source1 = source[['Date',item]]
+
+    train = source1[source1['Date'] <= pd.to_datetime("2024-01-31", format='%Y-%m-%d')]
+    test = source1[source1['Date'] > pd.to_datetime("2024-01-31", format='%Y-%m-%d')]
+
+    import pmdarima as pm
+
+    # Assuming your time series data is in 'data'
+    model = pm.auto_arima(train[item], seasonal=True, m=7,  # assuming monthly seasonality
+                        start_p=1, max_p=3,  # range for AR order
+                        start_q=1, max_q=3,  # range for MA order
+                        start_P=1, max_P=2,  # range for seasonal AR order
+                        start_Q=1, max_Q=2,  # range for seasonal MA order
+                        max_order=None,  # max order of differencing
+                        d=1, D=1,  # differencing order
+                        trace=True,  # print status of the fit
+                        error_action='ignore',  # ignore orders that don't converge
+                        suppress_warnings=True,  # suppress warnings
+                        stepwise=True)  # use stepwise algorithm for faster computation
+
+    print(model.summary())
+
+    # Getting the best parameters
+    best_order = model.order
+    best_seasonal_order = model.seasonal_order
+
+    best_parameters[counter,0:3] = best_order
+    best_parameters[counter,3:7] = best_seasonal_order
+    counter=counter+1
+
+
+np.savetxt('BestParameters.csv', best_parameters, delimiter=',')
---
+++ RequestForecasting hour.py
@@ -0,0 +1,55 @@
+import numpy as np
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from statsmodels.tsa.arima.model import ARIMA
+
+
+source = pd.read_csv("Req_Hourly_JB_BP.csv")
+source['Date Hour'] = pd.to_datetime(source['Date Hour'], format='%d/%m/%Y %H:%M')
+
+Vehicle_Type = source.columns[1:]
+parameters = np.genfromtxt('BestParameters.csv', delimiter=',')
+
+train = source[source['Date Hour'] <= pd.to_datetime("2024-01-31 00:00:00", format='%Y-%m-%d %H:%M:%S')]
+test = source[source['Date Hour'] > pd.to_datetime("2024-01-31 00:00:00", format='%Y-%m-%d %H:%M:%S')]
+
+Pred_result = test.copy()
+Pred_result.loc[:,Pred_result.columns != 'Date Hour']=0
+
+counter=0
+#StatModel SARIMAX
+for vehicle in Vehicle_Type:
+    y = train[vehicle]
+    p, d, q, P, D, Q, s = parameters[counter,0:]
+    SARIMAXmodel = SARIMAX(y, order = (p, d, q), seasonal_order=(P,D,Q,s))
+    SARIMAXmodel = SARIMAXmodel.fit()
+
+    y_pred_SARIMAX = SARIMAXmodel.get_forecast(len(test.index))
+    y_pred_df_SARIMAX = y_pred_SARIMAX.conf_int(alpha = 0.05) 
+    y_pred_df_SARIMAX["Predictions"] = SARIMAXmodel.predict(start = y_pred_df_SARIMAX.index[0], end = y_pred_df_SARIMAX.index[-1])
+    y_pred_df_SARIMAX.index = test.index
+    y_pred_out_SARIMAX = y_pred_df_SARIMAX["Predictions"]
+    Pred_result.loc[:,vehicle] = y_pred_out_SARIMAX 
+    counter=counter+1
+
+Pred_result.to_csv('HourlyForecast.csv', index=False)
+
+'''
+sns.set()
+plt.plot(train['Date'],train['Economy'], color = "black",label = 'Training')
+plt.plot(test['Date'],test['Economy'], color = "red",label = 'Testing')
+plt.plot(test['Date'],y_pred_out_ARMA, color='green', label = 'ARMA Predictions')
+plt.plot(test['Date'],y_pred_out_ARIMA, color='yellow', label = 'ARIMA Predictions')
+plt.plot(test['Date'],y_pred_out_SARIMAX, color='blue', label = 'SARIMAX Predictions')
+plt.ylabel('Number of Request')
+plt.xlabel('Date')
+plt.xticks(rotation=45)
+plt.subplots_adjust(bottom=0.2)
+plt.xlim(test['Date'].min(), test['Date'].max())
+plt.title("Train/Test split for Request")
+plt.legend()
+plt.show()
+'''
